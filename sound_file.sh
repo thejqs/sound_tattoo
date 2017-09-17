d650949017dceee544f24375a18d5d3c9ec5040a
Jacob Sanders <j.q.sanders@gmail.com>
2017-09-17 15:35:50 -0400

    adds comments

--- sound_file.sh
+++ sound_file.sh
@@ -1,7 +1,12 @@
 #!/usr/bin/env zsh
 
-curl https://mediadownloads.mlb.com/mlbam/2015/09/02/mlbtv_439507583_1800K.mp4 --output vin.mp4
-ffmpeg -i vin.mp4 -vn -acodec copy vin.aac
-ffmpeg -ss 6 -t 4 -i vin.aac short_vin.aac
+# grabs the target video file
+curl https://mediadownloads.mlb.com/mlbam/2015/09/02/mlbtv_439507583_1800K.mp4 --output av_files/vin.mp4
+# copies only the audio stream
+ffmpeg -i av_files/vin.mp4 -vn -acodec copy av_files/vin.aac
+# edits it down to the snippet we need -- four seconds beginning
+# at the sixth second
+ffmpeg -ss 6 -t 4 -i av_files/vin.aac av_files/short_vin.aac
 
+# fires up the Python script to turn tat snippet into an image 
 ~/Development/virtualenvs/sound_tattoo/bin/python3 sound_graph.py
--- sound_graph.py
+++ sound_graph.py
@@ -1,5 +1,11 @@
 #!usr/bin/env python3
 
+'''
+Having used curl to download the file and ffmpeg to edit it, this script
+is the last step in making a soundwave graph I can use
+as the basis for the artwork
+'''
+
 import scipy.io.wavfile
 import pydub
 import numpy as np
@@ -9,22 +15,50 @@ import matplotlib
 matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 
-ORIG_VIN_FILE = 'short_vin.aac'
-VIN_WAV = 'vin.wav'
+ORIG_VIN_FILE = 'av_files/short_vin.aac'
+VIN_WAV = 'av_files/vin.wav'
 
 
 def convert_to_wav(filepath):
+    '''
+    given a source audio file in aac format, converts it to an uncompressed wav
+
+    args:
+    string, a filepath
+
+    returns:
+    none, just leaves the output file in a predictable place
+    '''
     audio_file = pydub.AudioSegment.from_file(filepath, 'aac')
     audio_file.export(VIN_WAV, format='wav')
 
 
 def read_wav(filepath):
+    '''
+    reads the uncompressed wav file for basic data we'll need to plot the image
+
+    args:
+    string, the path to the wav file
+
+    returns:
+    a tuple, the file's bitrate and the audio data
+    '''
     bitrate, audio_data = scipy.io.wavfile.read(filepath)
 
     return bitrate, audio_data
 
 
 def plot_data(bitrate, data):
+    '''
+    given the file data, creates a plot and saves the resulting image
+
+    args:
+    int, the bitrate
+    obj, a scipy object with the audio data
+
+    returns:
+    none; terminates with the writing out of the image file
+    '''
     time = np.arange(0, float(data.shape[0]), 1) / bitrate
     plt.figure(1)
     plt.subplot(211)
@@ -35,6 +69,10 @@ def plot_data(bitrate, data):
 
 
 def hi_everybody(filepath):
+    '''
+    runs the jewels
+    runs the jewels fast
+    '''
     convert_to_wav(filepath)
     bitrate, audio_data = read_wav(VIN_WAV)
     plot_data(bitrate, audio_data)
